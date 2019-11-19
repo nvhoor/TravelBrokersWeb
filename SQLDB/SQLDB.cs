358bcf59120957cc95151a6c99c60c1ca70a2063
nvhoor <nvhoor@gmail.com>
2019-11-19 16:23:41 +0700

    commit connectionString create

---
+++ SQLDB/SQLDB.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    namespace SQLDB
+    {
+        public static class SQLDB
+        {
+
+            private static string connectionString = "";
+            public static string ConnectionString
+            {
+                get
+                {
+                    return connectionString;
+                }
+
+                set
+                {ss
+                    connectionString = value;
+                }
+            }
+            public static SqlConnection getConnection()
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                return conn;
+
+            }
+
+
+            public static int ExcuteNonQuery(SqlCommand cmd)
+            {
+                if (cmd.Connection != null)
+                {
+                    return cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    using (SqlConnection conn = getConnection())
+                    {
+                        cmd.Connection = conn;
+                        return cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            public static DataTable getData(SqlCommand cmd)
+            {
+                if (cmd.Connection != null)
+                {
+                    using (DataSet ds = new DataSet())
+                    {
+                        using (SqlDataAdapter da = new SqlDataAdapter())
+                        {
+                            da.SelectCommand = cmd;
+                            da.Fill(ds);
+                            return ds.Tables[0];
+                        }
+                    }
+                }
+                else
+                {
+                    using (SqlConnection conn = getConnection())
+                    {
+                        using (DataSet ds = new DataSet())
+                        {
+                            using (SqlDataAdapter da = new SqlDataAdapter())
+                            {
+                                cmd.Connection = conn;
+                                da.SelectCommand = cmd;
+                                da.Fill(ds);
+                                return ds.Tables[0];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
