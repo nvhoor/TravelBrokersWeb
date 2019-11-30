e31afd15ab78ea3a7ea2086148b0a136df260f54
Nora-Nguyen <46129319+utthao@users.noreply.github.com>
2019-11-30 10:07:35 +0700

    khoi tao du lieu cho bang Account, Tourrist, TourSupplies

--- SQLTravelBrokers.sql
+++ SQLTravelBrokers.sql
@@ -1,1 +1,24 @@
 use [TravelBrokers]
+--Accounts
+insert into Accounts values('utthao','123',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+insert into Accounts values('phuc','1234',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+insert into Accounts values('ho','12345',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+insert into Accounts values('nhi','123456',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+insert into Accounts values('quang','1234567',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+--Evaluations
+--PostCategories
+--Post
+--Price
+--Provinces
+--TourBookings
+--TourCategories
+--TourCustomers
+--Tourists
+insert into Tourists values('quang','01/01/1998',0);
+
+--TouristType
+--TourPrograms
+--Tours
+--TourSuppliers
+--Roles
+insert into
