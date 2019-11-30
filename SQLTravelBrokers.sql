3d46f66b51a01d3a91b2ef73740161b1080ce2a5
Nora-Nguyen <46129319+utthao@users.noreply.github.com>
2019-11-30 10:35:48 +0700

    coomit

--- SQLTravelBrokers.sql
+++ SQLTravelBrokers.sql
@@ -1,10 +1,10 @@
-use [TravelBrokers]
+﻿use [TravelBrokers]
 --Accounts
-insert into Accounts values('utthao','123',null,'utthaonguyenthi98@gmail.com','0901321202',0);
-insert into Accounts values('phuc','1234',null,'utthaonguyenthi98@gmail.com','0901321202',0);
-insert into Accounts values('ho','12345',null,'utthaonguyenthi98@gmail.com','0901321202',0);
-insert into Accounts values('nhi','123456',null,'utthaonguyenthi98@gmail.com','0901321202',0);
-insert into Accounts values('quang','1234567',null,'utthaonguyenthi98@gmail.com','0901321202',0);
+insert into Accounts values('utthao','123',null,'utthaonguyenthi98@gmail.com','0901321202','RO01',0);
+insert into Accounts values('phuc','1234',null,'utthaonguyenthi98@gmail.com','0901321202','RO02',0);
+insert into Accounts values('nhi','12345',null,'utthaonguyenthi98@gmail.com','0901321202','RO03',0);
+insert into Accounts values('ho','123456',null,'utthaonguyenthi98@gmail.com','0901321202','RO01',0);
+insert into Accounts values('quang','1233',null,'utthaonguyenthi98@gmail.com','0901321202','RO03',0);
 --Evaluations
 --PostCategories
 --Post
@@ -14,11 +14,17 @@ insert into Accounts values('quang','1234567',null,'utthaonguyenthi98@gmail.com'
 --TourCategories
 --TourCustomers
 --Tourists
-insert into Tourists values('quang','01/01/1998',0);
-
+insert into Tourists values('utthao','TO01','12/15/1998',0);
+insert into Tourists values('phuc','TO02','02/04/1998',0);
+insert into Tourists values('quang','TO03','08/16/1998',0);
+insert into Tourists values('nhi','TO04','02/06/1998',0);
+insert into Tourists values('ho','TO05','02/04/1998',0);
 --TouristType
 --TourPrograms
 --Tours
 --TourSuppliers
+insert into TourSuppliers values('utthao','TOS01','123456',null,'Hai Chau - Da Nang',0);
 --Roles
-insert into
+insert into Roles values('RO01','Quản trị viên');
+insert into Roles values('RO02','Nhà cung cấp');
+insert into Roles values('RO03','Khách hàng');
