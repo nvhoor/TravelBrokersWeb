356e03f818cc6887f7ac80994eeb6ab67d2e328f
n-accio <truongthihongnhi1998@gmail.com>
2019-12-07 18:27:05 +0700

    Thêm lệnh delete cho Prices, TourPrograms, Tours

--- TravelBrokersWeb/App_Code/TourProgramsHandler.cs
+++ TravelBrokersWeb/App_Code/TourProgramsHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,12 @@ namespace TravelBrokersWeb.App_Code
 {
     public class TourProgramsHandler
     {
+        public void deleteTourProgram(string idTourProgram)
+        {
+            SqlCommand cmd = new SqlCommand("delete from TourPrograms where id=@id");
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@id", idTourProgram);
+            SQLDB.SQLDB.ExcuteNonQuery(cmd);
+        }
     }
 }
