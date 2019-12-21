61cad0da9df359f3ad8d7236870533dc33a601d4
nvhoor <nvhoor@gmail.com>
2019-12-21 10:23:53 +0700

    no message

--- TravelBrokersWeb/App_Code/PricesHandler.cs
+++ TravelBrokersWeb/App_Code/PricesHandler.cs
@@ -19,7 +19,20 @@ namespace TravelBrokersWeb.App_Code
         }
         public void updatePrice(string id, string tourID, decimal originalPrice, decimal promotionPrice, DateTime StartDatePro, DateTime EndDatePro, string touristTypeID)
         {
-            SqlCommand cmd = new SqlCommand("update SANPHAM set tourID=@tourID,originalPrice=@originalPrice,promotionPrice=@promotionPrice,StartDatePro=@StartDatePro,EndDatePro=@EndDatePro,touristTypeID=@touristTypeID where id=@id");
+            SqlCommand cmd = new SqlCommand("update Prices set tourID=@tourID,originalPrice=@originalPrice,promotionPrice=@promotionPrice,StartDatePro=@StartDatePro,EndDatePro=@EndDatePro,touristTypeID=@touristTypeID where id=@id");
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@tourID", tourID);
+            cmd.Parameters.AddWithValue("@originalPrice", originalPrice);
+            cmd.Parameters.AddWithValue("@promotionPrice", promotionPrice);
+            cmd.Parameters.AddWithValue("@StartDatePro", StartDatePro);
+            cmd.Parameters.AddWithValue("@EndDatePro", EndDatePro);
+            cmd.Parameters.AddWithValue("@touristTypeID", touristTypeID);
+            SQLDB.SQLDB.ExcuteNonQuery(cmd);
+        }
+        public void insertPrice(string id, string tourID, decimal originalPrice, decimal promotionPrice, DateTime StartDatePro, DateTime EndDatePro, string touristTypeID)
+        {
+            SqlCommand cmd = new SqlCommand("insert Prices values (@id,@tourID,@originalPrice,@promotionPrice,@StartDatePro,@EndDatePro,@touristTypeID ");
             cmd.CommandType = CommandType.Text;
             cmd.Parameters.AddWithValue("@id", id);
             cmd.Parameters.AddWithValue("@tourID", tourID);
