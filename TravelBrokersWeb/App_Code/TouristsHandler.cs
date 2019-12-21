e0bf6c526617c8af069543d187afb85035e8a62a
Văn Hổ Nguyễn <nvhoor@gmail.com>
2019-12-22 00:33:08 +0700

    add TouristsHandler.cs

---
+++ TravelBrokersWeb/App_Code/TouristsHandler.cs
@@ -0,0 +1,21 @@
+using SQLDB;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TravelBrokersWeb.App_Code
+{
+    public class TouristsHandler
+    {
+        public void deleteTourists(string Tourists)
+        {
+            SqlCommand cmd = new SqlCommand("delete from Tourists where id=@id");
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@id", Tourists);
+            SQLDB.SQLDB.ExcuteNonQuery(cmd);
+        }
+    }
+}
